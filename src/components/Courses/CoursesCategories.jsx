905481916d2560a251758069173a9cd3e1b6d015
arcadyrajeev <bitmasteratj@gmail.com>
2025-09-01 00:08:56 +0530

    new

--- src/components/Courses/CoursesCategories.jsx
+++ src/components/Courses/CoursesCategories.jsx
@@ -30,7 +30,7 @@ export default function CoursesCategories() {
   };
 
   return (
-    <div className="categories hidden lg:flex flex-col w-[30vw] h-[80vh] rounded-lg">
+    <div className=" hidden lg:flex flex-col w-[30vw] h-[80vh] rounded-lg">
       <h2 className="headings">Courses</h2>
       <ul className="categories__list">
         <li
