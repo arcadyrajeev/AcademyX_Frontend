3db2c1c9dd66e07bc1e6e7aea593be6289c2f4f1
arcadyrajeev <bitmasteratj@gmail.com>
2025-09-02 15:29:47 +0530

    new

--- src/components/CourseCreation/CreateCourse.jsx
+++ src/components/CourseCreation/CreateCourse.jsx
@@ -74,7 +74,7 @@ const CreateCourse = () => {
   };
 
   return (
-    <div className="min-h-screen w-full bg-gray-50 px-4 py-8">
+    <div className="min-h-screen w-full  p-4">
       <div className="w-full mx-auto bg-white shadow-lg rounded-2xl p-8">
         <h1 className="text-3xl font-bold mb-6 text-gray-800">
           Create a New Course
